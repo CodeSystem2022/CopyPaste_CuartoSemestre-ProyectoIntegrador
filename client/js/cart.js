189d4d6e963b05a6bf1af51dc328bb822c6e8494
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2023-10-28 22:28:23 +0200

    Header del modal del carrito

---
+++ client/js/cart.js
@@ -0,0 +1,19 @@
+const modalContainer = document.getElementById("modal-container");
+const modalOverlay = document.getElementById("modal-overlay");
+
+const cart8tn = document.getElementById("cart-btn");
+
+
+const displayCart = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "block";
+    modalOverlay.style.display = "block";
+    //modal Header
+    const modalHeader = document.createElement("div");
+
+    const modalClose = document.createElement("div");
+    modalClose.innerText = "❌";
+    modalClose.className = "modal-close";
+    modalHeader.append(modalClose);
+
+};
