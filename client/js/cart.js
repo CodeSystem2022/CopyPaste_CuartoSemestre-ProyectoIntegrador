6d258ef14b39258f06222a4c51702e7906e75e1b
Jesica Viola <jesicaviola21@gmail.com>
2023-10-29 13:21:49 +0100

    Footer de la cart

--- client/js/cart.js
+++ client/js/cart.js
@@ -49,7 +49,15 @@ const displayCart = () => {
     `;
 
         modalContainer.append(modalBody);
-    })
+    });
+
+    //modal footer
+    const modalFooter = document.createElement("div");
+    modalFooter.className = "modal-footer";
+    modalFooter.innerHTML = `
+    <div class = "total-price">Total :)</div>`;
+
+    modalContainer.append(modalFooter);
 };
 
 cartBtn.addEventListener("click", displayCart);
