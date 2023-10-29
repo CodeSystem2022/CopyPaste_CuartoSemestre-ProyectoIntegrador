67793bddf8984aec6115f3796143a689669878bc
Jesica Viola <jesicaviola21@gmail.com>
2023-10-29 13:17:55 +0100

    Configuramos las cantidades de productos

--- client/js/index.js
+++ client/js/index.js
@@ -2,6 +2,7 @@ const shopContent = document.getElementById("shopContent");
 
 productos.forEach((product) =>{
     const content = document.createElement("div");
+    content.className = "card";
     content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName}</h3>
@@ -15,13 +16,22 @@ productos.forEach((product) =>{
     content.append(buyButton);
 
     buyButton.addEventListener("click", ()=>{
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        })
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+        if (repeat) {
+            cart.map((prod) => {
+                if (prod.id === product.id){
+                    prod.quanty++;
+                }
+            });            
+        }else{
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+        }
         console.log(cart)
-    })
+    });
 });
