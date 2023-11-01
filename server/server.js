07f96bd3ad4a48132a40f59e43b23362b7086bd9
jesica-viola <jesicaviola21@gmail.com>
2023-11-01 20:13:14 -0300

    Metodo de pago. Se prueban los pagos

--- server/server.js
+++ server/server.js
@@ -1,31 +1,29 @@
-const express = require("express"); //express: framework para node js
-const app = express(); //esta constante inicializa express
-const cors = require("cors"); //medida de seguridad para navegadores
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const mercadopago = require("mercadopago");
-const path = require("path"); //Direccion a los directorios necesarios para arrancar el servidor
+const path = require("path");
 
 // REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
 mercadopago.configure({
-    access_token: "<ACCESS_TOKEN>", //las creamos desde mercado pago después
+    access_token: "APP_USR-8005534317430798-091718-b982966f21e5ad67337aec6868ccafbb-1483636744",
 });
-
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); //para que pueda leer archivos .json
-app.use(express.static(path.join(__dirname, "../client"))); //no redirige a este archivo estático
-
+app.use(express.static(path.join(__dirname, "../client")));
 app.use(cors());
 
 //inicializa una primer ruta vacía "/"
 app.get("/", function () {
-   res.sendFile(path.resolve(__dirname, "..", "client", "index.html"));
-});
+   path.resolve(__dirname, "..", "client", "index.html");
+   });
 
-//al mandar desde el front-end los datos de una compra, se crea una preferencia
+
 app.post("/create_preference", (req, res) => {
 
     let preference = {
-        items: [ //con determinados items
+        items: [
             {
                 title: req.body.description, //título
                 unit_price: Number(req.body.price), //precio y desde donde lo trae
@@ -33,28 +31,28 @@ app.post("/create_preference", (req, res) => {
             }
         ],
         back_urls: {
-            "success": "http://localhost:8080", //si la compra se realizó con éxito, redirige al servidor localhost:8080
-            "failure": "http://localhost:8080", //si falla, volvemos a la página principal
-            "pending": "" //si está pendiente queda en blanco, NO lo usaremos
+            "success": "http://localhost:8080",
+            "failure": "http://localhost:8080",
+            "pending": ""
         },
-        auto_return: "approved", //te retorna al servidor
+        auto_return: "approved",
     };
 
     mercadopago.preferences.create(preference)
         .then(function (response) {
-            res.json({ //se manda al back-end la respuesta en formato json
-                id: response.body.id //se genera un ID para esa compra que se generó en el front-end
-            }); //ese id mostrará la información de la compra en la pasarela de pago
-        }).catch(function (error) { //usamos catch para manejo de errores
+            res.json({
+                id: response.body.id
+            });
+        })
+        .catch(function (error) {
         console.log(error);
     });
 });
 
-//CONFIGURACIONES AVANZADAS SOBRE EL ESTADO DEL PAGO -> NO LAS USAREMOS
-app.get('/feedback', function (req, res) { //para dar mas feedback en caso de configurar mas cosas en mercado pago
+app.get('/feedback', function (req, res) {
     res.json({
-        Payment: req.query.payment_id, //seguir el estado del pago
-        Status: req.query.status, //seguir el estado de la compra
+        Payment: req.query.payment_id,
+        Status: req.query.status,
         MerchantOrder: req.query.merchant_order_id
     });
 });
